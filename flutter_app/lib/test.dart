73955c228bab0d9364acf9d88e6b3bf54c134e3b
zhangxiaolong6 <zhangxiaolong6@xiaomi.com>
2019-06-27 14:04:59 +0800

    add json parse qsbk_hot_pic
    Signed-off-by: zhangxiaolong6 <zhangxiaolong6@xiaomi.com>

--- flutter_app/lib/test.dart
+++ flutter_app/lib/test.dart
@@ -1,39 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_app/QsbkHotPicItem.dart';
+import 'package:flutter_app/QsbkHotPicItemList.dart';
+import 'dart:convert';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      title: 'MyTitle',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: new MyStateful(title:'title'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'title'),
     );
   }
 }
 
-class MyStateful extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
-  MyStateful({Key key, this.title}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return new MyState();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class MyState extends State<MyStateful>{
-  String content = 'www';
+class _MyHomePageState extends State<MyHomePage> {
+  QsbkHotPicItemList mQsbkHotPicItemList;
 
   void getHttp() async {
     try {
-      Response response = await Dio().get("http://zxltest.zicp.vip:36619/test/qsbk_hot_pic/list?page=0&page_size=20");
-      print(response.data);
-      content = response.data;
+      Response response = await Dio().get("http://zxltest.zicp.vip:36619/test/qsbk_hot_pic/list?page=0&page_size=100");
+
+      mQsbkHotPicItemList = new QsbkHotPicItemList.fromJson(json.decode(response.data));
+      print('mQsbkHotPicItemList.length = ');
+      print(mQsbkHotPicItemList.itemList.length);
+      mQsbkHotPicItemList.itemList.forEach((qsbkHotPicItem){
+        print(qsbkHotPicItem.authorNickName + "---" + qsbkHotPicItem.content);
+      });
+
       setState(() {
 
       });
@@ -43,94 +70,48 @@ class MyState extends State<MyStateful>{
   }
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new MaterialButton(
-            child: Text('click',style: TextStyle(color: Colors.red),),
-            onPressed: getHttp,
-          ),
-          Text(
-            content,
-            style: TextStyle(
-              fontSize: 14
-            ),
-          )
-        ],
-      )
-    );
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getHttp();
   }
 
-}
-
-//class MyStateless extends StatelessWidget{
-//  @override
-//  Widget build(BuildContext context) {
-//    // TODO: implement build
-//    return Center(
-//      child: Text(
-//        'my content text',
-//        style: TextStyle(
-//            fontSize: 14,
-//            color: Colors.redAccent,
-//            backgroundColor: Colors.greenAccent
-//        ),
-//      ),
-//    );
-//  }
-//}
-
-class MyStateless extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: ListView(
-        children: <Widget>[
-          Text(
-            'text---1',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 14,
-                color: Colors.redAccent,
-                backgroundColor: Colors.greenAccent),
-          ),
-          Text(
-            'text---2',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 14,
-                color: Colors.redAccent,
-                backgroundColor: Colors.greenAccent),
-          ),
-          Text(
-            'text---3',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 14,
-                color: Colors.redAccent,
-                backgroundColor: Colors.greenAccent),
-          ),
-          Container(
-            height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('Entry A')),
-          ),
-          Container(
-            height: 50,
-            color: Colors.amber[500],
-            child: const Center(child: Text('Entry B')),
-          ),
-          Container(
-            height: 50,
-            color: Colors.amber[100],
-            child: const Center(child: Text('Entry C')),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
+      body: Center(
+        child :
+          mQsbkHotPicItemList == null ?
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new MaterialButton(
+                    child: Text('loading...',style: TextStyle(color: Colors.blue),)
+                  ),
+                ],
+              )
+          )
+              :
+          ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                QsbkHotPicItem qsbkHotPicItem = mQsbkHotPicItemList.itemList.elementAt(index);
+                return new Text(
+                  "$index --- ${qsbkHotPicItem.authorNickName}",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 14,
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return new Container(height: 1.0, color: Colors.blue);
+              },
+              itemCount: mQsbkHotPicItemList.itemList.length)
+      ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
