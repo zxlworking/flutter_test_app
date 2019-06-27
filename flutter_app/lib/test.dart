268c0f2c5d50c055e167c3cdcaf63e42d118eea9
zxlworking <szzhangxiaolong@163.com>
2019-06-27 08:05:59 +0800

    add click button to request http
    Signed-off-by: zxlworking <szzhangxiaolong@163.com>

--- flutter_app/lib/test.dart
+++ flutter_app/lib/test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,9 +10,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyTitle',
       theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: new MyStateless(),
+      home: new MyStateful(title:'title'),
     );
   }
+}
+
+class MyStateful extends StatefulWidget{
+  final String title;
+  MyStateful({Key key, this.title}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new MyState();
+  }
+}
+
+class MyState extends State<MyStateful>{
+  String content = 'www';
+
+  void getHttp() async {
+    try {
+      Response response = await Dio().get("http://zxltest.zicp.vip:36619/test/qsbk_hot_pic/list?page=0&page_size=20");
+      print(response.data);
+      content = response.data;
+      setState(() {
+
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new MaterialButton(
+            child: Text('click',style: TextStyle(color: Colors.red),),
+            onPressed: getHttp,
+          ),
+          Text(
+            content,
+            style: TextStyle(
+              fontSize: 14
+            ),
+          )
+        ],
+      )
+    );
+  }
+
 }
 
 //class MyStateless extends StatelessWidget{
